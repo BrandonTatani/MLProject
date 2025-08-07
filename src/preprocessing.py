94666319a79aa6f4014aa7b3b94c5b79cd223429
Brandon Tatani <brandon.tatani@outlook.com>
2025-08-07 18:39:17 +0200

    Loaded dataset

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,17 @@
 import pandas as pd
+from settings import DATA_DIR
+
+def concat_dataset():
+    """Dataset was already split and kaggle load was not working"""
+
+    df_1 = pd.read_csv(DATA_DIR / 'train.csv')
+    df_2 = pd.read_csv(DATA_DIR / 'validation.csv')
+    df_3 = pd.read_csv(DATA_DIR / 'test.csv')
+
+    _df = pd.concat([df_1, df_2, df_3])
+    _df.to_csv(DATA_DIR / 'dataset.csv', index=False)
 
 if __name__ == '__main__':
-    print('Hello World')
+
+    df = pd.read_csv(DATA_DIR / 'dataset.csv')
+    print(df.shape)
---
+++ src/settings.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / 'data'
+# Create paths like this BASE_DIR / 'subdir'
+
