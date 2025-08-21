c490f3479c52f8c96ce8494448279472f7874da7
Brandon Tatani <brandon.tatani@outlook.com>
2025-08-21 11:40:14 +0200

    Fixes to eval_pre_train.py

--- src/eval_pre_train.py
+++ src/eval_pre_train.py
@@ -1,6 +1,6 @@
 from settings import *
 from transformers import DataCollatorForSeq2Seq, AutoModelForSeq2SeqLM, AutoTokenizer
-from preprocessing import Preprocessor
+from preprocessing import Preprocessor, tokenizer, model
 import evaluate
 
 #!!! non so come aggiornare il file requirements ma bisogna installare evaluate
@@ -8,16 +8,11 @@ import evaluate
 #Inizializzazione
 preprocess = Preprocessor()
 
-# Carica modello e tokenizer
-print("\rLoading tokenizer...", end='')
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-print("\rLoading model...", end='')
-model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
-
 # Utilizzare il DataCollator per generare batch uniformi
 data_collator = DataCollatorForSeq2Seq(
     tokenizer=tokenizer,
     model=model,
+    label_pad_token_id=-100,
 )
 
 #ROUGE per valutare il modello BART pre-trained
@@ -35,4 +30,5 @@ def compute_metrics(eval_pred):
 
     return result
 
-eval_dataset = preprocess.splits['validation']
+if __name__ == "__main__":
+    eval_dataset = preprocess.splits['validation']
