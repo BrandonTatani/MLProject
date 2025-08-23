725c8a4053bc94f5b1e02ed96a74c300c1168606
Brandon Tatani <brandon.tatani@outlook.com>
2025-08-23 18:29:27 +0200

    Training

--- src/Evaluate.py
+++ src/Evaluate.py
@@ -1,34 +1,46 @@
-from torch.utils.hipify.hipify_python import preprocessor
-from transformers import Trainer, TrainingArguments, Seq2SeqTrainer,AutoTokenizer, AutoModelForSeq2SeqLM
-import evaluate
-
+from transformers import Trainer, Seq2SeqTrainingArguments, Seq2SeqTrainer, DataCollatorForSeq2Seq
+from preprocessing import Preprocessor, tokenizer, model
 from src.eval_pre_train import compute_metrics
+from settings import BASE_DIR
+import torch
 
-training_args = TrainingArguments(
-    output_dir = "./output",
-    evaluation_strategy = "epoch",
+torch.set_num_threads(8) # using multiple threads based on CPU
+
+preprocessed = Preprocessor()
+
+collator = DataCollatorForSeq2Seq(
+    tokenizer=tokenizer,
+    model=model,
+    label_pad_token_id=-100,
+)
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir = BASE_DIR / "output",
     save_strategy = "epoch",
+    eval_strategy="steps",
+    eval_steps=500,
     learning_rate = 5e-5,
-    per_device_train_batch_size = 2,
-    per_device_eval_batch_size = 2,
-    num_train_epochs = 1,
+    per_device_train_batch_size = 4,
+    per_device_eval_batch_size = 4,
+    num_train_epochs = 1, # maximum number of epochs
     weight_decay = 0.01,
     save_total_limit = 2,
     predict_with_generate = True,
-    logging_dir="./logs",
-    logging_steps=10,
-    overwrite_output_dir=True
+    logging_dir= BASE_DIR / "logs",
+    overwrite_output_dir=True,
+    report_to="none",
 )
 
 trainer = Seq2SeqTrainer(
     model = model,
     args = training_args,
-    train_dataset = tokenized_splits["train"],
-    eval_dataset = tokenized_splits["validation"],
+    train_dataset = preprocessed.train(),
+    eval_dataset = preprocessed.eval(),
+    data_collator = collator,
     tokenizer = tokenizer,
     compute_metrics = compute_metrics
 )
 
 trainer.train()
-results = trainer.evaluate()
+results = trainer.evaluate(preprocessed.test())
 print(results)
